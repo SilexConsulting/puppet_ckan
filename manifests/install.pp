5272ac9d801c01bb36846b4cb5b6eed0cfe79fe9
David Otton <david@otton.org>
2014-10-29 18:28:53 +0000

    Add dependencies on apache and apache mod wsgi

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,6 +1,8 @@
 class ckan::install(){
   include dgu_solr
   include uwsgi
+  include apache
+  include apache::mod::wsgi
 
   if ! defined(Package['libxslt1-dev'])       { package { 'libxslt1-dev':       ensure => present } }
   if ! defined(Package['libpq-dev'])          { package { 'libpq-dev':          ensure => present } }
