64d72f6d757d4dc30151fc7cd6d6535ec4387810
Noel Sharpe <noels@radnetwork.co.uk>
2015-02-19 22:00:58 +0000

    Add nginx config.

--- manifests/vhost.pp
+++ manifests/vhost.pp
@@ -2,7 +2,7 @@ class ckan::vhost (
 
 ) {
   apache::vhost { 'ckan':
-    port                        => 80,
+    port                        => 8000,
     docroot                     => '/var/ckan/',
     logroot                     => $ckan_log_root,
     wsgi_application_group      => '%{GLOBAL}',
@@ -12,6 +12,18 @@ class ckan::vhost (
       { process-group => 'wsgi', application-group => '%{GLOBAL}' },
     wsgi_process_group          => 'wsgi',
     custom_fragment             => 'WSGIPassAuthorization On',
-    wsgi_script_aliases         => { '/' => '/var/ckan/wsgi_app.py'},
+    wsgi_script_aliases         => { '/data' => '/var/ckan/wsgi_app.py'},
   }
+  nginx::resource::vhost { 'ckan':
+    proxy_redirect => 'http://ckan/ http://$host/',
+    proxy_set_header => ['X-Real-IP  $remote_addr', 'X-Forwarded-For $remote_addr', 'Host $host'],
+    proxy => 'http://upstream-ckan',
+  }
+
+  nginx::resource::upstream { 'upstream-ckan':
+    members => [
+      'localhost:8000',
+    ],
+  }
+
 }
